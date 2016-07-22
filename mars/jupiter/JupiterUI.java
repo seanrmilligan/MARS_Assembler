f3c3bfa8980b4bcb59a9ed7d8d7fd705b84ad0ec
Sean Milligan <seanrmilligan@gmail.com>
2016-07-22 09:14:36 -0400

    Replaces VenusUI with FX interface

---
+++ mars/jupiter/JupiterUI.java
@@ -0,0 +1,18 @@
+package mars.jupiter;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Sean on 2016-07-16.
+ */
+public class JupiterUI extends Application {
+	public void init() {
+		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+}
