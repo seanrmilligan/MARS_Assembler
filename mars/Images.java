6a4728b4472a09c33ae2859ce80e36313b427af1
Sean Milligan <seanrmilligan@gmail.com>
2016-08-04 16:43:58 -0400

    Globally accessible class that manages image resource locations

---
+++ mars/Images.java
@@ -0,0 +1,30 @@
+package mars;
+
+/**
+ * Created by Sean on 2016-08-04.
+ */
+public class Images {
+	private static final String IMAGE_DIRECTORY = "/images/";
+	public static final String MARS_SPLASH_IMAGE = Images.IMAGE_DIRECTORY + "MarsSurfacePathfinder.jpg";
+	
+	public static final String NEW_IMAGE = Images.IMAGE_DIRECTORY + "New16.png",
+			OPEN_IMAGE = Images.IMAGE_DIRECTORY + "Open16.png",
+			SAVE_IMAGE = Images.IMAGE_DIRECTORY + "Save16.png",
+			SAVE_AS_IMAGE = Images.IMAGE_DIRECTORY + "SaveAs16.png",
+			DUMP_MEMORY_IMAGE = Images.IMAGE_DIRECTORY + "Dump16.png",
+			PRINT_IMAGE = Images.IMAGE_DIRECTORY + "Print16.gif",
+			UNDO_IMAGE = Images.IMAGE_DIRECTORY + "Undo16.png",
+			REDO_IMAGE = Images.IMAGE_DIRECTORY + "Redo16.png",
+			CUT_IMAGE = Images.IMAGE_DIRECTORY + "Cut16.gif",
+			COPY_IMAGE = Images.IMAGE_DIRECTORY + "Copy16.png",
+			PASTE_IMAGE = Images.IMAGE_DIRECTORY + "Paste16.png",
+			FIND_REPLACE_IMAGE = Images.IMAGE_DIRECTORY + "Find16.png",
+			ASSEMBLE_IMAGE = Images.IMAGE_DIRECTORY + "Assemble16.png",
+			RUN_IMAGE = Images.IMAGE_DIRECTORY + "Play16.png",
+			FORWARD_STEP_IMAGE = Images.IMAGE_DIRECTORY + "StepForward16.png",
+			BACK_STEP_IMAGE = Images.IMAGE_DIRECTORY + "StepBack16.png",
+			PAUSE_IMAGE = Images.IMAGE_DIRECTORY + "Pause16.png",
+			STOP_IMAGE = Images.IMAGE_DIRECTORY + "Stop16.png",
+			RESET_IMAGE = Images.IMAGE_DIRECTORY + "Reset16.png",
+			HELP_IMAGE = Images.IMAGE_DIRECTORY + "Help16.png";
+}
